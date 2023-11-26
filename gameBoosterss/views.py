d60521d3d334a6060dea61af010c98502c8f9a62
sarahmohamed73 <sarahmoab73@gmail.com>
2023-11-26 21:37:13 +0200

    Homepage

--- gameBoosterss/views.py
+++ gameBoosterss/views.py
@@ -2,5 +2,57 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  games = ["League of legends", "Valorant", "Tft", "League of legends wild rift","Dota 2" ,"Hearthstone","World of warcraft", "Mobile legends", "Pubg mobile","Rocket league"]
-  return render(request, 'homepage/index.html', context={'games': games})
+  games = [
+    {
+      "id" : 1,
+      "name": "League of Legends",
+      "image": "League of legends.jpeg"
+    },
+    {
+      "id": 2,
+      "name": "Valorant",
+      "image": "Valorant.jpg"
+    },
+    {
+      "id": 3,
+      "name": "TFT",
+      "image": "tft.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Wild Rift",
+      "image": "League of legends wild rift.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Dota 2",
+      "image": "Dota 2.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Hearthstone",
+      "image": "Hearthstone.jpg"
+    },
+    {
+      "id": 7,
+      "name": "World of Warcraft",
+      "image": "World of Warcraft.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Mobile Legends",
+      "image": "Mobile Legends.webp"
+    },
+    {
+      "id": 9,
+      "name": "Pubg Mobile",
+      "image": "Pubg Mobile.avif"
+    },
+    {
+      "id": 10,
+      "name": "Rocket League",
+      "image": "Rocket League.webp"
+    },
+  ]
+
+  return render(request, 'homepage/index.html', context={"games": games})
