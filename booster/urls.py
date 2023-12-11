aed494feb916b8b3fa6d9fda23d4f8935b43264f
shehab <shethr999@gmail.com>
2023-12-11 04:55:01 +0200

    create rate model and rate view for order when end

--- booster/urls.py
+++ booster/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from booster.views import register_booster_view, profile_booster_view
+from booster.views import *
 
 urlpatterns = [
     path('register/',register_booster_view,name='booster.register'),
     path('profile/',profile_booster_view,name='booster.profile'),
+    path('rate/<int:order_id>/',get_rate,name='booster.rate'), # form to get rate if order done
+    path('form_test/', form_test, name='form_test'), # test page , only for test
 ]
