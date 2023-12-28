1c6e842ffe8f59148b00a8603f263e77c62e8326
shehab <shethr999@gmail.com>
2023-12-28 02:13:02 +0200

    fix extend order with booster

--- accounts/order_creator.py
+++ accounts/order_creator.py
@@ -20,7 +20,6 @@ def create_order(invoice, payer_id, customer, status='New'):
     extend_order_game_reached_rank = 0
     extend_order_game_reached_division = 0
     extend_order_game_reached_marks = 0
-
     try:
         extend_order = BaseOrder.objects.get(id=extend_order_id)
         extend_order.is_done = True
@@ -28,6 +27,11 @@ def create_order(invoice, payer_id, customer, status='New'):
         extend_order_money_owed = extend_order.money_owed
         extend_order_price = extend_order.price
         order_name = extend_order.name
+        extend_order_booster = extend_order.booster
+        extend_order_customer_gamename = extend_order.customer_gamename
+        extend_order_customer_password = extend_order.customer_password
+        extend_order_server = extend_order.customer_server
+        extend_order_data_correct = extend_order.data_correct
         extend_order.money_owed = 0
         extend_order.save()
         extend_order_game = Game.objects.get(order = extend_order)
@@ -51,12 +55,18 @@ def create_order(invoice, payer_id, customer, status='New'):
     order.save_with_processing()
     baseOrder.price = extend_order_price + baseOrder.price
     baseOrder.money_owed = extend_order_money_owed + baseOrder.money_owed
-    baseOrder.save()
     if extend_order_game_reached_rank != 0 and extend_order_game_reached_division != 0 and extend_order_game_reached_marks != 0:
         order.reached_rank = extend_order_game_reached_rank
         order.reached_division = extend_order_game_reached_division
         order.reached_marks = extend_order_game_reached_marks
+        baseOrder.booster = extend_order_booster
+        baseOrder.customer_gamename = extend_order_customer_gamename
+        baseOrder.customer_password = extend_order_customer_password
+        baseOrder.customer_server = extend_order_server
+        baseOrder.data_correct = extend_order_data_correct
+        baseOrder.status = 'Extend'
         order.save()
+    baseOrder.save()
     baseOrder.customer_wallet()
     print(f'Order: {order}')
     return order        
