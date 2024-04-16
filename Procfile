ff1c04bc6d35afb52bcbbaf34fa8a43320aa6d53
shehab <shethr999@gmail.com>
2024-04-16 08:01:18 +0200

    make imports insde asgi lasy import, to enable websocket in server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne -b 0.0.0.0 -p $PORT gameBoosterss.asgi:application
+daphne -b 0.0.0.0 -p 8000 gameBoosterss.asgi:application
--- gameBoosterss/asgi.py
+++ gameBoosterss/asgi.py
@@ -5,14 +5,23 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameBoosterss.settings')
  
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter , URLRouter
-from chat import routing as chatRouting
-from accounts import routing as orderRouting
+# from chat import routing as chatRouting
+# from accounts import routing as orderRouting
+
+def get_websocket_urlpatterns():
+    # Import the routing modules here
+    from chat import routing as chatRouting
+    from accounts import routing as orderRouting
+    
+    # Combine the websocket_urlpatterns from both modules
+    return chatRouting.websocket_urlpatterns + orderRouting.websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
         "http" : get_asgi_application() ,
         "websocket" : AuthMiddlewareStack(
             URLRouter(
-                orderRouting.websocket_urlpatterns + chatRouting.websocket_urlpatterns
+                get_websocket_urlpatterns()
             )   
         )
     }
