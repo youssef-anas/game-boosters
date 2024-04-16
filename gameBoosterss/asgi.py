0c877d46fa7420134dfd26cd14fd03a3c3140947
shehab <shethr999@gmail.com>
2024-04-16 08:10:31 +0200

    edit asgi file for lazy import as module

--- gameBoosterss/asgi.py
+++ gameBoosterss/asgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
- 
+import importlib
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameBoosterss.settings')
  
 from channels.auth import AuthMiddlewareStack
@@ -9,12 +10,16 @@ from channels.routing import ProtocolTypeRouter , URLRouter
 # from accounts import routing as orderRouting
 
 def get_websocket_urlpatterns():
-    # Import the routing modules here
-    from chat import routing as chatRouting
-    from accounts import routing as orderRouting
+    # Import the routing modules as strings
+    chatRouting_module = importlib.import_module('chat.routing')
+    orderRouting_module = importlib.import_module('accounts.routing')
+    
+    # Get the websocket_urlpatterns from both modules
+    chat_websocket_urlpatterns = getattr(chatRouting_module, 'websocket_urlpatterns', [])
+    order_websocket_urlpatterns = getattr(orderRouting_module, 'websocket_urlpatterns', [])
     
     # Combine the websocket_urlpatterns from both modules
-    return chatRouting.websocket_urlpatterns + orderRouting.websocket_urlpatterns
+    return chat_websocket_urlpatterns + order_websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
