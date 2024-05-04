85ea2b45edbc7521c903af28dbcc964b14874b67
sarahmohamed73 <sarahmoab73@gmail.com>
2024-05-05 01:15:18 +0200

    Edition

--- static/base/js/base.js
+++ static/base/js/base.js
@@ -1,8 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $toggleMenu = $('.main-nav .toggle-menu');
   $links = $('.main-nav .links');
-  $toggleMenu.click(function() {
+  $toggleMenu.click(function () {
     $links.toggleClass('shown')
   })
+
+  // Function to enable/disable the submit button for a specific form
+  function toggleSubmitButton(form, disable) {
+    let submitButton = form.find('button[type="submit"]');
+    let submitInput = form.find('input[type="submit"]');
+
+    submitButton.prop('disabled', disable);
+    submitInput.prop('disabled', disable);
+  }
+
+  // Disable submit buttons initially for all forms
+  $('form').each(function() {
+    toggleSubmitButton($(this), true);
+  });
+
+  // Add event listener to form fields
+  $('form').on('input change', function () {
+    // Enable submit button for the changed form
+    toggleSubmitButton($(this), false);
+  });
+
+  // Add event listener to submit buttons
+  $('form').on('submit', function () {
+    var form = $(this);
+    // Disable submit button after form submission
+    setTimeout(function() {
+      toggleSubmitButton(form, true);
+    }, 0); // Use setTimeout to ensure the button is disabled after form submission
+  });
+
 
 });
